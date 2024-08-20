d75ab433c00fd98b8a2e7495c210237671f4e199
Jake Tran <jake@Rays-Mac-mini.local>
2024-08-20 16:32:05 +0100

    Remove Copilot

--- lua/community.lua
+++ lua/community.lua
@@ -8,8 +8,6 @@ return {
 
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.utility.noice-nvim" },
-  { import = "astrocommunity.completion.copilot-lua" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.vim-move" },
